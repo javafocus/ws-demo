1501efb51155d075533a494a47fd136b04e43b3c
Lingarajkumar Srinivasaperumal <lingapsg@Lingarajkumars-MacBook-Pro.local>
2019-06-10 18:54:20 +0200

    add SOAP web service implementation using spring

---
+++ ws-client/src/main/java/com/demo/ws/client/WsClientApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ws.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsClientApplication.class, args);
+	}
+
+}
